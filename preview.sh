31cb40840e35eb190357a397968dbb84efab1b96
sethjuarez <me@sethjuarez.com>
2020-11-02 15:38:24 -0800

    added better ignore criteria for watch

--- preview.sh
+++ preview.sh
@@ -1,2 +1,8 @@
 pip install sphinx-autobuild
-sphinx-autobuild --ignore *.png  -D plot_gallery=0 -b html "." "_build/html"
+sphinx-autobuild --ignore "*.png" \
+                 --ignore "advanced/*" \
+                 --ignore "beginner/*" \
+                 --ignore "intermediate/*" \
+                 --ignore "prototype/*" \
+                 --ignore "recipes/*" \
+                 -D plot_gallery=0 -b html "." "_build/html"
